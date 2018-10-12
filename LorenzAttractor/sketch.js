9b756b08c0a630f16383c55d144138f348c7da57
Kirill Matienko <kirill@ego-cms.com>
2018-10-12 18:35:36 +0300

    Generate fixed attractor and then draw it

--- LorenzAttractor/sketch.js
+++ LorenzAttractor/sketch.js
@@ -1,19 +1,22 @@
 var attractor;
 var angle = 0;
 var hu = 0;
+var isGrowing = true;
+var currentPoint = 0;
 
 
 function setup() {
     createCanvas(500, 500, WEBGL);
     colorMode(HSB, 255, 255, 255);
-    attractor = new Attractor();
+    attractor = new Attractor(5000, 1);
+    attractor.initDefault();
+    attractor.generate();
     //attractor.createVar1();
 }
 
 function draw() {
     background(20);
     scale(3.5);
-    attractor.step();
     angle += 0.01;
 
     rotate(angle, [2,1.5,1]);
@@ -21,12 +24,22 @@ function draw() {
 
     stroke(hu, 255, 255);
     beginShape();
-    for (var i = 0; i < attractor.points.length; i++){
+    for (var i = 0; i < currentPoint; i++){
         c = attractor.points[i];
         vertex(c.x, c.y, c.z);
         
     }
     endShape();
+
+    if(isGrowing)
+        currentPoint ++;
+    else
+        currentPoint --;
+
+    if(currentPoint == attractor.points.length)
+        isGrowing = false;
+    else if(currentPoint == 0)
+        isGrowing = true
 
     hu += 0.1;
     if(hu >= 255){
